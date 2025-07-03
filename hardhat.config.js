8a76ff59a0597dd3017ff890c0541accff8dbd0f
Alain Nicolas <alain.nicolas@consensys.net>
2025-07-03 09:53:56 +0200

    feat: enable verification on Lineascan

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,30 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+
+require("@nomicfoundation/hardhat-verify");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./.env" });
+
 module.exports = {
   solidity: "0.8.21",
+  networks: {
+    linea: {
+      url: "https://rpc.linea.build",
+    },
+  },
+  etherscan: {
+    apiKey: {
+      linea: process.env.LINEASCAN_API_KEY ?? "",
+    },
+    customChains: [
+      {
+        network: "linea",
+        chainId: 59144,
+        urls: {
+          apiURL: "https://api.lineascan.build/api",
+          browserURL: "https://lineascan.build",
+        },
+      }
+    ],
+  },
 };
